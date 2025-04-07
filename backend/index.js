f6a974cbc38313c362241a2278b45e277d661226
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-04-07 11:02:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json({ limit: '300mb' }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/DemoDB', {
+mongoose.connect('mongodb://localhost:27017/Your databse Name', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
